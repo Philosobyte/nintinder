b4736b24d8422b6d8d95811408c46b8e25c936de
Johan Pereira <jwpereira@live.com>
2018-03-15 19:21:24 -0400

    added UserAdmin to admin.py

--- tikkachu/nintinder/admin.py
+++ tikkachu/nintinder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Game, Interest, Achievement, EarnedAchievement, Event, Participant
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'last_name', 'first_name')
+    fields = [('last_name', 'first_name'), 'email', 'username', 'date_of_birth', 'gender']
