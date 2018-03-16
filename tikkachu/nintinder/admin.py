01449962557ce489e62a44f46f371950c052ad5e
Johan Pereira <jwpereira@live.com>
2018-03-16 00:08:34 -0400

    created basic admin classes for models

--- tikkachu/nintinder/admin.py
+++ tikkachu/nintinder/admin.py
@@ -6,4 +6,35 @@ from .models import User, Game, Interest, Achievement, EarnedAchievement, Event,
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'last_name', 'first_name')
-    fields = [('last_name', 'first_name'), 'email', 'username', 'date_of_birth', 'gender']
+    fields = ['id', ('last_name', 'first_name'), 'email',
+              'username', 'date_of_birth', 'gender', 'location']
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'platform', 'release_date')
+
+
+@admin.register(Interest)
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'game')
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'name')
+
+
+@admin.register(EarnedAchievement)
+class EarnedAchievementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'achievement')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'start_time', 'end_time')
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'event', 'user')
