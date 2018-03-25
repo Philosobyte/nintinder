2b318b4478a40006d2615c94866fb0defa56f88c
Johan Pereira <jwpereira@live.com>
2018-03-25 19:26:08 -0400

    might delete soon but I felt friendA and friendB were cute
    (friendA and friendB are related_name attributes that are likely to get replaced in the future).
    (The commit message was supposed to be memey lolol)

--- tikkachu/nintinder/models.py
+++ tikkachu/nintinder/models.py
@@ -72,8 +72,8 @@ class Participant(models.Model):
 
 class Friend(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    friend1 = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Enter the id of the user to whom this friends list belongs")
-    friend2 = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Enter the id of the user who is on the friends list")
+    friendA = models.ForeignKey(User, related_name='friendA', on_delete=models.CASCADE, help_text="Enter the id of the user to whom this friends list belongs")
+    friendB = models.ForeignKey(User, related_name='friendB', on_delete=models.CASCADE, help_text="Enter the id of the user who is on the friends list")
 
     STATUSES = (
         (u'0', u'friends'),
