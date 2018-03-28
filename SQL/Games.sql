fb9e89fab84997312e76178d5a8b79af802fdb07
Oliver Pang <opang23@gmail.com>
2018-03-28 16:11:48 -0400

    Added sql files for populating Games/Achievements/EarnedAchievenets/Interests
    Boo

---
+++ SQL/Games.sql
@@ -0,0 +1,33 @@
+INSERT INTO nintinder_game VALUES ('c464e2b5-eedf-488a-a53c-56fd067c0ee4', 'Counter-Strike: Global Offensive', 'PC', 'Valve Corporation', 'The game pits two teams against each other: the Terrorists and the Counter-Terrorists. Both sides are tasked with eliminating the other while also completing separate objectives, the Terrorists, depending on the game mode, must either plant the bomb or defend the hostages, while the Counter-Terrorists must either prevent the bomb from being planted, defuse the bomb, or rescue the hostages. There are eight game modes, all of which have distinct characteristics specific to that mode.', '2012-08-21');
+
+INSERT INTO nintinder_game VALUES ('79da553b-3c15-4dc5-a07f-146097304b54', 'League of Legends', 'PC', 'Riot Games', 'League of Legends (abbreviated LoL) is a multiplayer online battle arena video game developed and published by Riot Games for Microsoft Windows and macOS.', '2009-10-27');
+
+INSERT INTO nintinder_game VALUES ('a3b0a58c-010b-4076-8b00-313e537013ad', 'Tom Clancy''s Rainbow Six Siege', 'PC', 'Ubisoft', 'Tom Clancy''s Rainbow Six Siege is a tactical shooter video game developed by Ubisoft Montreal and published by Ubisoft.', '2015-12-01');
+INSERT INTO nintinder_game VALUES ('94f53507-184d-4d15-849e-814f90864e9a', 'Tom Clancy''s Rainbow Six Siege', 'Xbox One', 'Ubisoft', 'Tom Clancy''s Rainbow Six Siege is a tactical shooter video game developed by Ubisoft Montreal and published by Ubisoft.', '2015-12-01');
+INSERT INTO nintinder_game VALUES ('8c8b100e-4367-4ae3-b6e1-63a892a62e48', 'Tom Clancy''s Rainbow Six Siege', 'PlayStation 4', 'Ubisoft', 'Tom Clancy''s Rainbow Six Siege is a tactical shooter video game developed by Ubisoft Montreal and published by Ubisoft.', '2015-12-01');
+
+INSERT INTO nintinder_game VALUES ('42880d2a-af71-416f-90ba-fae0ef52f0ff', 'Super Smash Bros. Brawl', 'Wii', 'Nintendo', 'Super Smash Bros. Brawl[a] is a crossover fighting game, the third installment in the Super Smash Bros. series, developed by Sora Ltd. and Game Arts and published by Nintendo for the Wii video game console.', '2008-01-31');
+
+INSERT INTO nintinder_game VALUES ('c52cfb32-3042-4fc3-9d78-6a93328b8317', 'Super Smash Bros. Melee', 'GameCube', 'Nintendo', 'Super Smash Bros. Melee is a crossover fighting video game developed by HAL Laboratory and published by Nintendo for the GameCube video game console. ', '2001-11-21');
+
+INSERT INTO nintinder_game VALUES ('acd664fd-1c56-4a8e-b09f-62ae88236325', 'Halo 3', 'Xbox 360', 'Microsoft Game Studios', 'Halo 3 is a 2007 first-person shooter video game developed by Bungie for the Xbox 360 console. The third installment in the Halo franchise, the game concludes the story arc begun in 2001''s Halo: Combat Evolved and continued in 2004''s Halo 2.', '2007-09-25');
+INSERT INTO nintinder_game VALUES ('8285b3a8-847d-485d-8202-7d893c85cb22', 'Halo 3', 'Xbox One', 'Microsoft Game Studios', 'Halo 3 is a 2007 first-person shooter video game developed by Bungie for the Xbox 360 console. The third installment in the Halo franchise, the game concludes the story arc begun in 2001''s Halo: Combat Evolved and continued in 2004''s Halo 2.', '2007-09-25');
+
+INSERT INTO nintinder_game VALUES ('2d9c199e-e854-441f-ab30-cce96c48f303', 'The Elder Scrolls V: Skyrim', 'PC', 'Bethesda Softworks', 'The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.', '2011-11-11');
+INSERT INTO nintinder_game VALUES ('8621e3e9-c60b-40f6-a47d-a68902c70d41', 'The Elder Scrolls V: Skyrim', 'PlayStation 3', 'Bethesda Softworks', 'The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.', '2011-11-11');
+INSERT INTO nintinder_game VALUES ('4f2af043-d511-452f-a350-a6ea37571df6', 'The Elder Scrolls V: Skyrim', 'PlayStation 4', 'Bethesda Softworks', 'The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.', '2016-10-28');
+INSERT INTO nintinder_game VALUES ('017b8507-5773-4fa2-b13b-103b38b441b9', 'The Elder Scrolls V: Skyrim', 'Xbox 360', 'Bethesda Softworks', 'The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.', '2011-11-11');
+INSERT INTO nintinder_game VALUES ('22fae007-2d42-4a93-a3a2-74f48093b117', 'The Elder Scrolls V: Skyrim', 'Xbox One', 'Bethesda Softworks', 'The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.', '2016-10-28');
+INSERT INTO nintinder_game VALUES ('d94e2cbe-ab78-4762-95c8-5a4adfd8cd68', 'The Elder Scrolls V: Skyrim', 'Nintendo Switch', 'Bethesda Softworks', 'The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.', '2017-11-17');
+
+
+INSERT INTO nintinder_game VALUES ('54fa90b3-729b-4766-ad69-125a40d3cf74', 'The Legend of Zelda: Breath of the Wild', 'Nintendo Switch', 'Nintendo', 'The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo.', '2017-03-03');
+INSERT INTO nintinder_game VALUES ('ecfc687b-c7e9-42a1-a3e1-a92fa45be3a2', 'The Legend of Zelda: Breath of the Wild', 'Wii U', 'Nintendo', 'The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo.', '2017-03-03');
+	
+
+
+
+
+
+
+
