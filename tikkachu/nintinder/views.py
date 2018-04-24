2a1e2fb686aee4b3766d697bb409b5b66ffb8aaa
Oliver Pang <opang23@gmail.com>
2018-04-24 00:34:33 -0400

    Added headers to and stylized profile/achievements
    yahaha!

--- tikkachu/nintinder/views.py
+++ tikkachu/nintinder/views.py
@@ -148,6 +148,7 @@ def settings(request):
             request,
             'settings.html',
             context={
+                'usr': usr,
                 'full_name': currName,
                 'uform': uform,
                 'pform': pform
