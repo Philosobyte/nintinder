93b3d9612d901be249c1a2ca2a056d3421dfbca1
Oliver Pang <opang23@gmail.com>
2018-04-30 20:49:35 -0400

    working email reset
    blah blah

--- tikkachu/tikkachu/settings.py
+++ tikkachu/tikkachu/settings.py
@@ -129,4 +129,9 @@ LOGIN_REDIRECT_URL = '/nintinder'
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tikkachuofficial@gmail.com'
+EMAIL_HOST_PASSWORD = 'nintinder' #pls no hack
+EMAIL_PORT = 587
